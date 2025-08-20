7cef109c38466d6c1a1e56180330eae957e8ebc0
Ramon Saviato <ramonsaviato@hotmail.com>
2025-08-20 20:02:26 -0300

    chore: project setup

---
+++ src/test/java/com/ramon/authentication/AuthenticationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
