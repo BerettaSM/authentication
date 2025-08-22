380e41fddf371c130247881dfd44c402beb2c660
Ramon Saviato <ramonsaviato@hotmail.com>
2025-08-21 21:48:16 -0300

    add PasswordEncoder

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-INSERT INTO TBL_USER (email, password) VALUES ('user@email.com', '123456');
+-- Password: password
+INSERT INTO TBL_USER (email, password) VALUES ('user@email.com', '$2a$10$DfFJ3oKzkfwmfoELVKXYr.BkSzOQ9lduiTICOi/wEa5FMclYvs7Ny');
