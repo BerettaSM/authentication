6ed97c33f012b84e836ce9dc1d3c763a31f5dfc1
Ramon Saviato <ramonsaviato@hotmail.com>
2025-08-23 23:07:34 -0300

    add custom argument resolver to inject authenticated user on secured endpoints

--- src/main/java/com/ramon/authentication/config/SecurityConfig.java
+++ src/main/java/com/ramon/authentication/config/SecurityConfig.java
@@ -1,19 +1,26 @@
 package com.ramon.authentication.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ramon.authentication.security.interceptors.AuthenticationHandlerInterceptor;
 import com.ramon.authentication.security.interceptors.AuthorizationTokenInterceptor;
+import com.ramon.authentication.security.resolvers.AuthenticationResolver;
 
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationResolver authenticationResolver;
 
     @Autowired
     private AuthorizationTokenInterceptor authorizationTokenInterceptor;
@@ -25,6 +32,11 @@ public class SecurityConfig implements WebMvcConfigurer {
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
         registry.addInterceptor(authorizationTokenInterceptor);
         registry.addInterceptor(authenticationHandlerInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(@NonNull List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticationResolver);
     }
 
     @Bean
