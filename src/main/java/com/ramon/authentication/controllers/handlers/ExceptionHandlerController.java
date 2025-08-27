62a7ed6285614b532351079ed5d0d3bc23521bc1
Ramon Saviato <ramonsaviato@hotmail.com>
2025-08-26 21:45:19 -0300

    add error handling for invalid paths

--- src/main/java/com/ramon/authentication/controllers/handlers/ExceptionHandlerController.java
+++ src/main/java/com/ramon/authentication/controllers/handlers/ExceptionHandlerController.java
@@ -1,11 +1,16 @@
 package com.ramon.authentication.controllers.handlers;
 
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.ramon.authentication.domain.dto.CustomError;
 import com.ramon.authentication.exceptions.ApplicationException;
+import com.ramon.authentication.utils.PathUtils;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
@@ -13,6 +18,16 @@ public class ExceptionHandlerController {
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<CustomError> catchAll(ApplicationException e) {
         CustomError err = CustomError.from(e);
+        return ResponseEntity.status(err.getStatus()).body(err);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<CustomError> invalidPath(NoResourceFoundException e) {
+        CustomError err = new CustomError(
+            HttpStatus.NOT_FOUND.value(), 
+            "Invalid path", 
+            Instant.now(), 
+            PathUtils.getCurrentPath());
         return ResponseEntity.status(err.getStatus()).body(err);
     }
 
